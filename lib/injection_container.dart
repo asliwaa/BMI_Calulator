1002d5629a459f73e7d73b89131b6189c66d0811
asliwaa <asliwa603@gmail.com>
2026-02-11 13:18:56 +0100

    Dependency injection

---
+++ lib/injection_container.dart
@@ -0,0 +1,6 @@
+import 'package:get_it/get_it.dart';
+import 'features/bmi/data/datasources/bmi_local_data_source.dart';
+import 'features/bmi/data/repositories/bmi_repository_impl.dart';
+import 'features/bmi/domain/repositories/bmi_repository.dart';
+import 'features/bmi/domain/usecases/calculate_bmi_usecase.dart';
+import 'features/bmi/presentation/bloc/bmi_bloc.dart';
